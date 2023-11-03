bbd24691ce871c3d5f147900c941bc0b73602666
Magnus Thomé <32169384+MagnusThome@users.noreply.github.com>
2023-11-03 11:10:34 +0100

    Update config.h

--- config.h
+++ config.h
@@ -1,8 +1,6 @@
-const char* hostname = "rainmeter";
-
 const char* ssid = "xxxxxxxx";
-const char* password = "xxxxxxxx";
-
+const char* wifipassword = "xxxxxxxx";
+const char* hostname = "rainmeter";
 
 const char* mqttserver = "xxxxxxxx";
 const char* mqtt_username = "xxxxxxx";
@@ -11,3 +9,7 @@ const char* mqtt_password = "xxxxxxxx";
 const char* ota_path = "/firmware";
 const char* ota_username = "xxxxxxx";
 const char* ota_password = "xxxxxx";
+
+const char* haTopic  = "rainmeter";
+const char* haName   = "Rainmeter";
+const char* haUniqid = "rainmeter01";
