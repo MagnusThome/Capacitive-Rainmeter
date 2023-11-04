7cfbf5454d31e301576eb42da61f9bef2684a071
Magnus Thomé <32169384+MagnusThome@users.noreply.github.com>
2023-11-04 11:12:00 +0100

    Update config.h

--- config.h
+++ config.h
@@ -13,3 +13,13 @@ const char* ota_password = "xxxxxx";
 const char* haTopic  = "rainmeter";
 const char* haName   = "Rainmeter";
 const char* haUniqid = "rainmeter01";
+
+#define CAPACITANCE_OFFSET  200     // Set so that your meter reports close to zero when fully dry
+
+#define GPIO_NTC        1
+#define GPIO_HEATER     48
+#define GPIO_CAPACITOR  2
+#define GPIO_1MOHM      41
+
+#define BLUE_LED        10
+#define YELLOW_LED      11
